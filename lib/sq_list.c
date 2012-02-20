076e39f4081081abfe8a4649ab9beb41087a6536
xxz <xxz@blue.(none)>
2012-02-20 03:52:42 -0500

    del all

--- lib/sq_list.c
+++
@@ -1,7 +0,0 @@
-#include"sq_list.h"
-void sq_list_init(SQ_LIST *sqlist)
-{
-    printf("this is sq_list libraries\n");
-    //sqlist->elem=malloc(sizeof(SQ_LIST)*LIST_INIT_SIZE);
-//    elem->
-}
--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include"sq_list.h"
-int main(int argc,char **argv)
-{
-    SQ_LIST *sql;
-    sq_list_init(sql);
-    return 0;
-}
