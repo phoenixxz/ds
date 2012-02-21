5cb14968f92bfa46d803c0ed15e28e8060ff6988
xxz <xxz@blue.(none)>
2012-02-20 23:05:04 -0500

    add quanpailie

--- chapter_1/3.c
+++ chapter_1/3.c
@@ -1,8 +1,40 @@
 /*
- * 折半查找到递归实现
+ * //quan pai lie
  *
  * */
+#include<stdio.h>
+void swap(char *m,char *n);
+void perm(char *list,int i,int n);
 void main()
 {
-    
+    char list[3]={'a','b','c'};
+    perm(list,0,2);
+}
+void perm(char *list,int i,int n)
+{
+    int j;
+    if(i==n)
+    {
+	for(j=0;j<=n;j++)
+	{
+	    printf("%c  ",list[j]);
+	}
+	printf("\n");
+    }
+    else
+    {
+	for(j=i;j<=n;j++)
+	{
+	    swap(&list[i],&list[j]);
+	    perm(list,i+1,n);
+	    swap(&list[i],&list[j]);
+	}
+    }
+}
+void swap(char *m,char *n)
+{
+    char t;
+    t=*m;
+    *m=*n;
+    *n=t;
 }
